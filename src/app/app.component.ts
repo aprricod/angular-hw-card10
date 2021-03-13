0a6c9864c2f821990bf610767ba4f199b43abb63
Artem <aprricod@gmail.com>
2021-03-13 16:12:35 +0300

    history added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'card10';
+  constructor(private router: Router) {}
+
+  // getHistory() {
+  //   this.router.events
+  //     .pipe(filter((event) => event instanceof NavigationEnd))
+  //     .subscribe(async ({ urlAfterRedirects }: NavigationEnd) => {
+  //       console.log(urlAfterRedirects);
+  //     });
+  // }
 }
