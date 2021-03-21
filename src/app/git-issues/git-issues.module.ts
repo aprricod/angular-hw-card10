20733f2617b28e91286681fae02d0f48948865a2
Artem <aprricod@gmail.com>
2021-03-21 23:38:24 +0300

    mat icon added

--- src/app/git-issues/git-issues.module.ts
+++ src/app/git-issues/git-issues.module.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -30,6 +31,7 @@ import { GitIssuesComponent } from './git-issues.component';
     MatCardModule,
     MatButtonModule,
     MatDialogModule,
+    MatIconModule,
   ],
   exports: [GitIssuesComponent, AddIssueComponent, AddCommentComponent],
   providers: [
